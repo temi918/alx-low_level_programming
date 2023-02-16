4a24929fe425edc2b4cf17a58a02295ae0b55da4
temi918 <topetemi918@gmail.com>
2023-02-16 15:16:24 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function that prints a given text
+ *
+ * Return: 0 when it is successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
