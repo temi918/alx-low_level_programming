926df32d7d78d2c71dab3e79395374cac40effec
temi918 <topetemi918@gmail.com>
2023-02-21 18:04:17 -0800

    Nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,26 +5,46 @@
  *
  * Return: Always 0 (Success)
  */
-int main() {
-    int current = 1;
-    int previous = 1;
-    int next;
-    int count = 2;
+int main(void)
+{
+	int count;
+	unsigned long f1 = 0, f2 = 1, sum;
+	unsigned long f1_half1, f1_half2, f2_half1, f2_half2;
+	unsigned long half1, half2;
 
-    printf("%d, %d, ", previous, current);
+	for (count = 0; count < 92; count++)
+	{
+		sum = f1 + f2;
+		printf("%lu, ", sum);
 
-    while (count < 98) {
-        next = current + previous;
-        previous = current;
-        current = next;
-        printf("%d, ", current);
-        count++;
-    }
+		f1 = f2;
+		f2 = sum;
+	}
 
-    next = current + previous;
-    previous = current;
-    current = next;
-    printf("%d\n", current);
+	f1_half1 = f1 / 10000000000;
+	f2_half1 = f2 / 10000000000;
+	f1_half2 = f1 % 10000000000;
+	f2_half2 = f2 % 10000000000;
 
-    return 0;
+	for (count = 93; count < 99; count++)
+	{
+		half1 = f1_half1 + f2_half1;
+		half2 = f1_half2 + f2_half2;
+		if (f1_half2 + f2_half2 > 9999999999)
+		{
+			half1 += 1;
+			half2 %= 10000000000;
+		}
+
+		printf("%lu%lu", half1, half2);
+		if (count != 98)
+			printf(", ");
+
+		f1_half1 = f2_half1;
+		f1_half2 = f2_half2;
+		f2_half1 = half1;
+		f2_half2 = half2;
+	}
+	printf("\n");
+	return (0);
 }
