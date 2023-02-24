65912569f5544c3bdbb375ce7df5e2af676ad552
temi918 <topetemi918@gmail.com>
2023-02-23 17:58:56 -0800

    hot fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,50 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_numbers - print integers using _putchar
+ * @n: integer to print
+ * Return: void
  */
-int main(void)
+void print_number(int n)
 {
-    print_number(98);
-    _putchar('\n');
-    print_number(402);
-    _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
-    print_number(0);
-    _putchar('\n');
-    print_number(-98);
-    _putchar('\n');
-    return (0);
+	char a, b, c, d, e, f, g, h, i, j;
+
+	a = ((n % 10) + '0');
+	b = ((n / 10) + '0');
+	c = (((n / 10) % 10) + '0');
+	d = (((n / 10) / 10) + '0');
+	e = ((((n / 10) / 10) / 10) + '0');
+	f = ((((n / 10) / 10) % 10) + '0');
+	g = (((n % 100) / 10) + '0');
+	h = (((n % 10) % 10) + '0');
+	i = ((n % 10) + '0' + 17);
+	j = ((n / 10) + '0' + 17);
+	if (n > 9 && n < 99)
+	{
+		_putchar(b);
+		_putchar(a);
+	}
+	if (n > 99 && n < 999)
+	{
+		_putchar(d);
+		_putchar(c);
+		_putchar(a);
+	}
+	if (n > 999)
+	{
+		_putchar(e);
+		_putchar(f);
+		_putchar(g);
+		_putchar(h);
+	}
+	if (n < 9 && n > -1)
+	{
+		_putchar(a);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		_putchar(i);
+		_putchar(j);
+	}
 }
