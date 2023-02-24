4781d727cd4dd3aa6ec4c393b702f5fe7327f906
temi918 <topetemi918@gmail.com>
2023-02-23 18:04:52 -0800

    hot fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,50 +2,23 @@
 void print_integer(int m);
 
 /**
- * print_numbers - print integers using _putchar
- * @n: integer to print
- * Return: void
+ * print_number - Prints an integer.
+ * @n: The integer
+ * Return: Nothing
  */
 void print_number(int n)
 {
-	char a, b, c, d, e, f, g, h, i, j;
+	// If number is smaller than 0, put a - sign
+	// and change number to positive
+	if (n < 0) {
+        	_putchar('-');
+        	n = -n;
+	}
 
-	a = ((n % 10) + '0');
-	b = ((n / 10) + '0');
-	c = (((n / 10) % 10) + '0');
-	d = (((n / 10) / 10) + '0');
-	e = ((((n / 10) / 10) / 10) + '0');
-	f = ((((n / 10) / 10) % 10) + '0');
-	g = (((n % 100) / 10) + '0');
-	h = (((n % 10) % 10) + '0');
-	i = ((n % 10) + '0' + 17);
-	j = ((n / 10) + '0' + 17);
-	if (n > 9 && n < 99)
-	{
-		_putchar(b);
-		_putchar(a);
-	}
-	if (n > 99 && n < 999)
-	{
-		_putchar(d);
-		_putchar(c);
-		_putchar(a);
-	}
-	if (n > 999)
-	{
-		_putchar(e);
-		_putchar(f);
-		_putchar(g);
-		_putchar(h);
-	}
-	if (n < 9 && n > -1)
-	{
-		_putchar(a);
-	}
-	if (n < 0)
-	{
-		_putchar('-');
-		_putchar(i);
-		_putchar(j);
-	}
+	// Remove the last digit and recur
+	if (n/10)
+		print(n/10);
+
+	// Print the last digit
+	_putchar(n%10 + '0');
 }
