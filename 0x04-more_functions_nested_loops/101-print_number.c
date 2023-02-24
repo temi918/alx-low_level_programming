7f97330b4d8ea76dcf6a3bc0e9e09b282e93d378
temi918 <topetemi918@gmail.com>
2023-02-23 17:49:56 -0800

    hot fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer.
- * @n: The integer
- * Return: Nothing
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void print_number(int n)
+int main(void)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n / 10)
-	{
-		print_number(n / 10);
-	}
-	_putchar(n % 10 + '0');
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
 }
