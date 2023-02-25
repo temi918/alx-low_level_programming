043e86c20c1208737e2bfbf599ab1e60c88eb543
temi918 <topetemi918@gmail.com>
2023-02-25 01:47:45 -0800

    c_programming

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - creating the main.c file for this directory
+ * Return: Always zero(success)
+ *
+ */
+
+int main(void)
+{
+	return (0);
+}
