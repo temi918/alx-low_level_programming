30e883253ad8d19e58329b3f5602a7b03cd58963
temi918 <topetemi918@gmail.com>
2023-02-25 16:39:10 -0800

    practice

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
  *
  * Return: 0
  */
+
 int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
+	for (num = 0; num <= 9; num++)
+		printf("%d", num);
 	printf("\n");
 
 	return (0);
