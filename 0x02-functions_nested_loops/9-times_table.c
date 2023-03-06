bdfa6093d57a826334b6d1665c762721642fde32
temi918 <topetemi918@gmail.com>
2023-03-05 18:03:02 -0800

    latest update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,25 +5,38 @@
  * Description: Prints 9 times table starting with 0
  * Return: Nothing.
  */
-
 void times_table(void)
 {
-    int i, j, product;
+	int i, j, n;
 
-    for (i = 0; i <= 9; i++) {
-        for (j = 0; j <= 9; j++) {
-            product = i * j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			n = i * j;
 
-            if (product < 10) {
-                _putchar(product + '0');
-                _putchar(' ');
-            }
-            else {
-                _putchar((product / 10) + '0');
-                _putchar((product % 10) + '0');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+			if ((n / 10) == 0)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(n + '0');
+
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
+
