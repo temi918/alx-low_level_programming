b9e44e9dd7f4b6cba8372d7ba94bea0addd3e54a
temi918 <topetemi918@gmail.com>
2023-03-08 17:43:55 -0800

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,12 +9,12 @@
 
 int _pow_recursion(int x, int y)
 {
-	  if (y < 0) 
-		  return (-1);
-	  else if (y == 0)
-		  return (1);
-	  else if (y == 1)
-		  return (x);
-	  return (x *= _pow_recursion(x, y-1));
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	return (x *= _pow_recursion(x, y - 1));
 }
 
