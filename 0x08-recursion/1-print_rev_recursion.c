20e65f8383c0792c3fb127758ce5515dbcea1091
temi918 <topetemi918@gmail.com>
2023-03-08 16:53:52 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - A function that prints a string in reverse
+ * followed by a new line
+ * @s: as an input string to printing
+ * Return: null.
+ *
+ */
+
+_print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	
+}
