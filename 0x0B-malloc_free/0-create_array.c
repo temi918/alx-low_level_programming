60b3a4086b29ba5a58d3974e82801bef652fddf3
temi918 <topetemi918@gmail.com>
2023-03-13 14:48:35 -0700

    malloc task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,10 +16,11 @@ char *create_array(unsigned int size, char c)
 
 	array = malloc(size * sizeof(char));
 
-	if (array == 0)
-		return (0);
+	if (size == 0)
+		return (NULL);
 	if (array == NULL)
 		return (NULL);
+
 	while (z < size)
 	{
 		array[z] = c;
