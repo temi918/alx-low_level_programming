fdbb2888781b6c46b0cce8ee00c34f553ee62298
temi918 <topetemi918@gmail.com>
2023-03-30 13:57:31 -0700

    Anither strange task

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,20 +1,21 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * free_list - frees a list.
- * @head: a list that enters to the function.
+ * free_list - A function that frees all elements in a linked list
+ * @head: pointer to head element of list
+ * Return: Nothing
  */
 void free_list(list_t *head)
 {
 	list_t *temp;
+
 	temp = head;
-	while (temp != NULL)
+	while (head)
 	{
+		temp = head;
+		head = head->next;
 		free(temp->str);
 		free(temp);
-		temp = temp->next;
 	}
+	free(head);
 }
