d097f459975f6f9dae8bdf25148b3a6d4dbbcd0f
temi918 <topetemi918@gmail.com>
2023-04-03 12:53:48 -0700

    singlylist

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,7 +19,7 @@ typedef struct listint_s
 } listint_t;
 void free_listint2(listint_t **head);
 size_t recursion_printer(const listint_t *h);
-size_t print_listint(const listint_t *q);
+size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
