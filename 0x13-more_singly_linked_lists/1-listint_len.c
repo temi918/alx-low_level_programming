ed5ecd40927eb4f4b93513843d9b424ba5ccf4de
temi918 <topetemi918@gmail.com>
2023-04-03 14:00:35 -0700

    singlylist

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,13 +3,12 @@
 
 /**
  *
- * listint_len - will return d number of elements in a linkedint list
+ * listint_len - returns d number of elements in a linkedint list
  *
  * @r: A pointer to all the listint_t structure
  *
  * Return: the number of all elements.
  */
-
 
 size_t listint_len(const listint_t *r)
 {
