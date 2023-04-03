91e2132700733fb7d5c81dab0bb2d5d5bb04951f
temi918 <topetemi918@gmail.com>
2023-04-03 12:50:57 -0700

    singlylist

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,10 +3,10 @@
 
 /**
  * 0-print_listint.c - would print all elements of a listint_t list
- * 
+ *
  * @q: a pointer to listint
  *
- * 
+ *
  * Return: total number of nodes
  */
 
@@ -14,7 +14,7 @@ size_t print_listint(const listint_t *q)
 {
 	size_t n = 0;
 
-	while(q != NULL)
+	while (q != NULL)
 	{
 		printf("%d\n", q->n);
 		q = q->next;
