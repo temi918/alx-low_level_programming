6277064e143f3bd8504ebf4d17c8041b50e2c996
temi918 <topetemi918@gmail.com>
2023-04-03 13:50:47 -0700

    singlylist

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-include "lists.h"
+#include "lists.h"
 
 /**
  * print_listint - prints all elements in the list
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -33,5 +33,5 @@ void free_listint(listint_t *head);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *add_nodeint(listint_t **head, const int n);
 size_t listint_len(const listint_t *h);
-size_t print_listint(const listint_t *r);
+size_t print_listint(const listint_t *h);
 #endif /* _LISTS_H_ */
