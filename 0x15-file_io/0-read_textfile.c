7a089b2569637bd102c6a01828d41a4dad51ed43
temi918 <topetemi918@gmail.com>
2023-04-10 07:22:23 -0700

    File Descriptors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,4 +5,50 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+/**
+ * read_textfile - A function that reads a text file & prints it to POSIX STANDARD OUTPUT
+ *
+ * @filename: Filename to open text
+ *
+ * @numOfLetters: The Number of letters it should read and print
+ *
+ * Return: The number of letters read and printed, or return 0.
+ *
+ */
 
+ssize_t read_textfile(const char *filename, size_t numOfLetters)
+{
+	int fdo, fdr, fdw;
+	char *temp;
+	int nut;
+	int vut, jut;
+
+	temp = malloc(sizeof(char) * numOfLetters);
+	if (temp == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	nut = open(filename, O_RDONLY);
+	if (nut < 0)
+	{
+		free(temp);
+		return (0);
+	}
+
+	fdr = read(nut, temp, numOfLetters);
+	if (fdr < 0)
+	{
+		free(temp);
+		return (0);
+	}
+
+	fdw = write(STDOUT_FILENO, temp, fdr);
+	free(temp);
+	close(nut);
+
+	if (fdw < 0)
+		return (0);
+	return ((ssize_t)fdw);
+}
