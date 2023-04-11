54b6ee9d39536714d10d4cff057b7ef498a8707b
temi918 <topetemi918@gmail.com>
2023-04-10 20:30:40 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,22 +1,32 @@
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/uio.h>
 
 int safe_close(int);
+
 /**
- * main - Main function to copy files
- * @argc: num of args passed
- * @argv: pointers to array args
- * Return: 1 on success, else exit
+ * main - The main function to copy files
+ *
+ * @argc: Takes the number of argumentss passed
+ *
+ * @argv: The pointer to array arguments
+ *
+ * Return: returns 1 on success, else jsut exit
+ *
  */
+
 int main(int argc, char *argv[])
 {
-	char buffer[1024];
-	int bytes_read = 0, _EOF = 1, from_fd = -1, to_fd = -1, error = 0;
+	char buf[1024];
+	int byts_r = 0;
+	int ENDOFFILE = 1;
+	int fd = -1;
+	int _fd = -1;
+	int error = 0;
 
 	if (argc != 3)
 	{
@@ -24,9 +34,51 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	from_fd = open(argv[1], O_RDONLY);
-	if (from_fd < 0)
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+
+	_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (_fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		safe_close(fd);
+		exit(99);
+	}
+
+	while (ENDOFFILE)
+	{
+		ENDOFFILE = read(fd, buf, 1024);
+		if (ENDOFFILE < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			safe_close(fd);
+			safe_close(_fd);
+			exit(98);
+		}
+		else if (ENDOFFILE == 0)
+			break;
+		byts_r += ENDOFFILE;
+		error = write(_fd, buf, ENDOFFILE);
+		if (error < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			safe_close(fd);
+			safe_close(_fd);
+			exit(99);
+		}
+	}
+	error = safe_close(_fd);
+	if (error < 0)
+	{
+		safe_close(fd);
+		exit(100);
+	}
+	error = safe_close(fd);
+	if (error < 0)
+		exit(100);
+	return (0);
+}
